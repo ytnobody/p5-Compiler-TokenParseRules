36134d803e578e47a940a49d3e6641bbb8250787
ytnobody <ytnobody@gmail.com>
2020-01-29 00:57:57 +0900

    add package name normalization feature

--- lib/Compiler/TokenParseRules/PackageName.pm
+++ lib/Compiler/TokenParseRules/PackageName.pm
@@ -50,4 +50,61 @@ sub handle_token {
     return ($token, $state);
 }
 
+sub after_handle {
+    my ($class, $tokens) = @_;
+    my @return_tokens = @$tokens;
+    use Data::Dumper;
+    my @namespace_definition_tokens = _extract_namespace_definition_tokens(@return_tokens);
+    for my $def_token (@namespace_definition_tokens) {
+        my ($begin, $end, $replace) = @$def_token;
+        @return_tokens = _apply_namespace_definition_tokens($begin, $end, $replace, @return_tokens);
+    }
+    return @return_tokens;
+}
+
+sub _extract_namespace_definition_tokens {
+    my @tokens = @_;
+    my @namespace_tokens_list = ();
+
+    my @namespace_tokens = undef;
+    my ($token, $i, $begin, $end);
+    for $i (0 .. $#tokens) {
+        $token = $tokens[$i];
+
+        if (!defined $begin && $token->{name} eq "Package") {
+            $begin = $i;
+            @namespace_tokens = ($token);
+        }
+        elsif (defined $begin) {
+            push @namespace_tokens, $token;
+        }
+
+        if (defined $begin && $token->{name} eq "SemiColon") {
+            $end = $i;
+            my $package_name = $token->{package};
+            push @namespace_tokens_list, [$begin, $end, [_fill_package($package_name, @namespace_tokens)]];
+            $begin = $end = undef;
+            @namespace_tokens = undef;
+        }
+    }
+
+    return @namespace_tokens_list;
+}
+
+sub _fill_package {
+    my ($package_name, @tokens) = @_;
+    map {$_->{package} = $package_name; $_} @tokens;
+}
+
+sub _apply_namespace_definition_tokens {
+    my ($begin, $end, $replace, @tokens) = @_;
+    my @return_tokens;
+    my $i;
+    for $i (0 .. $#tokens) {
+        my $row = $i >= $begin && $i <= $end ? $replace->[$i-$begin] : $tokens[$i];
+        push @return_tokens, $row;
+    }
+    return @return_tokens;
+}
+
 1;
